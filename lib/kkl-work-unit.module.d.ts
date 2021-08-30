d059335e563cdd2b7e7d309611601695ced747da
uriokkl <88084814+uriokkl@users.noreply.github.com>
2021-08-30 15:32:50 +0300

    Delete lib directory

--- lib/kkl-work-unit.module.d.ts
+++
@@ -1,7 +0,0 @@
-import * as i0 from "@angular/core";
-import * as i1 from "./kkl-work-unit.component";
-export declare class KklWorkUnitModule {
-    static ɵfac: i0.ɵɵFactoryDeclaration<KklWorkUnitModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<KklWorkUnitModule, [typeof i1.KklWorkUnitComponent], never, [typeof i1.KklWorkUnitComponent]>;
-    static ɵinj: i0.ɵɵInjectorDeclaration<KklWorkUnitModule>;
-}
--- lib/kkl-work-unit.service.d.ts
+++
@@ -1,6 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class KklWorkUnitService {
-    constructor();
-    static ɵfac: i0.ɵɵFactoryDeclaration<KklWorkUnitService, never>;
-    static ɵprov: i0.ɵɵInjectableDeclaration<KklWorkUnitService>;
-}
