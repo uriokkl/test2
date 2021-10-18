e5300757c9cebb24874217ed1c89f99a8a83f076
uriokkl <88084814+uriokkl@users.noreply.github.com>
2021-10-18 09:35:49 +0300

    Add files via upload

---
+++ fesm2015/yaaranut-gis.js
@@ -0,0 +1,346 @@
+import * as i0 from '@angular/core';
+import { Injectable, EventEmitter, Component, ViewChild, Output, Input, NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { __awaiter } from 'tslib';
+import WebMap from '@arcgis/core/WebMap';
+import MapView from '@arcgis/core/views/MapView';
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
+import Basemap from '@arcgis/core/Basemap';
+import LabelClass from '@arcgis/core/layers/support/LabelClass';
+import { SimpleFillSymbol } from '@arcgis/core/symbols';
+import Color from '@arcgis/core/Color';
+import SimpleRenderer from '@arcgis/core/renderers/SimpleRenderer';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+class WorkUnitService {
+    constructor() { }
+}
+WorkUnitService.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: WorkUnitService, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+WorkUnitService.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: WorkUnitService, providedIn: 'root' });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: WorkUnitService, decorators: [{
+            type: Injectable,
+            args: [{
+                    providedIn: 'root'
+                }]
+        }], ctorParameters: function () { return []; } });
+
+class WorkUnitComponent {
+    constructor() {
+        this.mapLoaded = new EventEmitter();
+        this._workUnits = [];
+        this.firstTime = true;
+        this._z = "z";
+        this.featerLayer = new FeatureLayer();
+        this.mapView = new MapView();
+    }
+    set content(content) {
+        if (content) {
+            this.mapViewEl = content;
+        }
+    }
+    set zz(zzz) {
+        this._z = zzz;
+    }
+    get zz() {
+        return this._z;
+    }
+    set workUnits(workUnits) {
+        this._workUnits = workUnits;
+        if (this.firstTime) {
+            this.firstTime = false;
+            this.initializeMap();
+        }
+        const WorkUnitsWhere = workUnits.map(workUnit => "'" + workUnit + "'").
+            join();
+        this.featerLayer.definitionExpression = "GlobalID in (" + WorkUnitsWhere + ")";
+        this.featerLayer.when(() => {
+            const query = this.featerLayer.createQuery();
+            query.outSpatialReference = this.mapView.spatialReference;
+            this.featerLayer.queryFeatures().then(response => {
+                response.features.forEach(feature => {
+                    const axzz = "Dfgd";
+                });
+            });
+            this.featerLayer.queryExtent(query)
+                .then(response => {
+                if (response.extent !== null) {
+                    response.extent.spatialReference = this.mapView.spatialReference;
+                    this.mapView.goTo(response.extent).catch(function (error) { console.error(error); });
+                }
+                var EsriPwoerByelements = document.getElementsByClassName("esri-ui calcite-theme-light");
+                EsriPwoerByelements[0].setAttribute("style", "display:none");
+            });
+        });
+    }
+    get workUnits() {
+        return this._workUnits;
+    }
+    initializeMap() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const webMap = new WebMap({
+                basemap: "topo",
+                //portalItem: {
+                //  //url:"https://services2.arcgis.com/utNNrmXb4IZOLXXs/ArcGIS/rest/services/JNFILForest/FeatureServer/0/query"
+                //  id: "streets"
+                //}
+            });
+            let basemap = new Basemap({
+                portalItem: {
+                    //url:""
+                    id: "streets" // WGS84 Streets Vector webmap
+                }
+            });
+            try {
+                this.featerLayer = new FeatureLayer({ url: "https://services2.arcgis.com/utNNrmXb4IZOLXXs/ArcGIS/rest/services/Test_KKLForestManagementUnits/FeatureServer/0/query" });
+                this.featerLayer.opacity = 0.5;
+                this.featerLayer.definitionExpression = "1=2";
+                //this.featerLayer.displayField = "FOR_NO";
+                //this.featerLayer.labelsVisible = true;
+                //this.featerLayer.legendEnabled = true;
+                //this.featerLayer.outFields = ["FOR_NO"];
+                //this.featerLayer.popupEnabled = true;
+                const featerRenderer = new SimpleRenderer();
+                featerRenderer.label = "{FOR_NO}";
+                const polygonsSimpleFillSymbol = new SimpleFillSymbol();
+                polygonsSimpleFillSymbol.color = Color.fromString("gold");
+                polygonsSimpleFillSymbol.outline.color = Color.fromString("blue");
+                polygonsSimpleFillSymbol.outline.width = 2;
+                featerRenderer.symbol = polygonsSimpleFillSymbol;
+                const labelClass = new LabelClass();
+                labelClass.labelExpressionInfo = { expression: "$feature.FOR_NO  " };
+                this.featerLayer.labelingInfo = [labelClass];
+                this.featerLayer.renderer = featerRenderer;
+                webMap.add(this.featerLayer);
+                this.mapView.container = this.mapViewEl.nativeElement;
+                this.mapView.map = webMap;
+                //(await mapView.whenLayerView(featerLayer)).filter.where = "GlobalID = '" + this._filter[0] + "'";
+                //mapView.when(() => {
+                //  this.mapLoaded.emit(true);
+                //});
+            }
+            catch (error) {
+                alert('We have an error: ' + error);
+            }
+        });
+    }
+    ngOnInit() {
+    }
+}
+WorkUnitComponent.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: WorkUnitComponent, deps: [], target: i0.ɵɵFactoryTarget.Component });
+WorkUnitComponent.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "12.0.0", version: "12.2.3", type: WorkUnitComponent, selector: "lib-workUnit", inputs: { zz: "zz", workUnits: "workUnits" }, outputs: { mapLoaded: "mapLoaded" }, viewQueries: [{ propertyName: "content", first: true, predicate: ["mapViewNode"], descendants: true, static: true }], ngImport: i0, template: `
+  <div #mapViewNode style="width:100%;height: 100%;background-color:yellow"></div>
+  `, isInline: true });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: WorkUnitComponent, decorators: [{
+            type: Component,
+            args: [{
+                    selector: 'lib-workUnit',
+                    template: `
+  <div #mapViewNode style="width:100%;height: 100%;background-color:yellow"></div>
+  `,
+                    styles: []
+                }]
+        }], ctorParameters: function () { return []; }, propDecorators: { content: [{
+                type: ViewChild,
+                args: ['mapViewNode', { static: true }]
+            }], mapLoaded: [{
+                type: Output
+            }], zz: [{
+                type: Input
+            }], workUnits: [{
+                type: Input
+            }] } });
+
+class WorkUnitModule {
+    set123() {
+    }
+}
+WorkUnitModule.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: WorkUnitModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
+WorkUnitModule.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: WorkUnitModule, declarations: [WorkUnitComponent], imports: [FormsModule,
+        BrowserModule], exports: [WorkUnitComponent] });
+WorkUnitModule.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: WorkUnitModule, imports: [[
+            FormsModule,
+            BrowserModule
+        ]] });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: WorkUnitModule, decorators: [{
+            type: NgModule,
+            args: [{
+                    declarations: [
+                        WorkUnitComponent
+                    ],
+                    imports: [
+                        FormsModule,
+                        BrowserModule
+                    ],
+                    exports: [
+                        WorkUnitComponent
+                    ],
+                    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+                }]
+        }] });
+
+class SeedsCollectService {
+    constructor() { }
+}
+SeedsCollectService.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: SeedsCollectService, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+SeedsCollectService.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: SeedsCollectService, providedIn: 'root' });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: SeedsCollectService, decorators: [{
+            type: Injectable,
+            args: [{
+                    providedIn: 'root'
+                }]
+        }], ctorParameters: function () { return []; } });
+
+class SeedsCollectComponent {
+    constructor() {
+        this.mapLoaded = new EventEmitter();
+        this._workUnits = [];
+        this.firstTime = true;
+        this._z = "z";
+        this.featerLayer = new FeatureLayer();
+        this.mapView = new MapView();
+    }
+    set content(content) {
+        if (content) {
+            this.mapViewEl = content;
+        }
+    }
+    set zz(zzz) {
+        this._z = zzz;
+    }
+    get zz() {
+        return this._z;
+    }
+    set workUnits(workUnits) {
+        this._workUnits = workUnits;
+        if (this.firstTime) {
+            this.firstTime = false;
+            this.initializeMap();
+        }
+        const WorkUnitsWhere = workUnits.map(workUnit => "'" + workUnit + "'").
+            join();
+        this.featerLayer.definitionExpression = "GlobalID in (" + WorkUnitsWhere + ")";
+        this.featerLayer.when(() => {
+            const query = this.featerLayer.createQuery();
+            query.outSpatialReference = this.mapView.spatialReference;
+            this.featerLayer.queryFeatures().then(response => {
+                response.features.forEach(feature => {
+                    const axzz = "Dfgd";
+                });
+            });
+            this.featerLayer.queryExtent(query)
+                .then(response => {
+                if (response.extent !== null) {
+                    response.extent.spatialReference = this.mapView.spatialReference;
+                    this.mapView.goTo(response.extent).catch(function (error) { console.error(error); });
+                }
+                var EsriPwoerByelements = document.getElementsByClassName("esri-ui calcite-theme-light");
+                EsriPwoerByelements[0].setAttribute("style", "display:none");
+            });
+        });
+    }
+    get workUnits() {
+        return this._workUnits;
+    }
+    initializeMap() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const webMap = new WebMap({
+                basemap: "topo",
+                //portalItem: {
+                //  //url:"https://services2.arcgis.com/utNNrmXb4IZOLXXs/ArcGIS/rest/services/JNFILForest/FeatureServer/0/query"
+                //  id: "streets"
+                //}
+            });
+            let basemap = new Basemap({
+                portalItem: {
+                    //url:""
+                    id: "streets" // WGS84 Streets Vector webmap
+                }
+            });
+            try {
+                this.featerLayer = new FeatureLayer({ url: "https://services2.arcgis.com/utNNrmXb4IZOLXXs/ArcGIS/rest/services/Test_KKLForestManagementUnits/FeatureServer/0/query" });
+                this.featerLayer.opacity = 0.5;
+                this.featerLayer.definitionExpression = "1=2";
+                //this.featerLayer.displayField = "FOR_NO";
+                //this.featerLayer.labelsVisible = true;
+                //this.featerLayer.legendEnabled = true;
+                //this.featerLayer.outFields = ["FOR_NO"];
+                //this.featerLayer.popupEnabled = true;
+                const featerRenderer = new SimpleRenderer();
+                featerRenderer.label = "{FOR_NO}";
+                const polygonsSimpleFillSymbol = new SimpleFillSymbol();
+                polygonsSimpleFillSymbol.color = Color.fromString("gold");
+                polygonsSimpleFillSymbol.outline.color = Color.fromString("blue");
+                polygonsSimpleFillSymbol.outline.width = 2;
+                featerRenderer.symbol = polygonsSimpleFillSymbol;
+                const labelClass = new LabelClass();
+                labelClass.labelExpressionInfo = { expression: "$feature.FOR_NO  " };
+                this.featerLayer.labelingInfo = [labelClass];
+                this.featerLayer.renderer = featerRenderer;
+                webMap.add(this.featerLayer);
+                this.mapView.container = this.mapViewEl.nativeElement;
+                this.mapView.map = webMap;
+                //(await mapView.whenLayerView(featerLayer)).filter.where = "GlobalID = '" + this._filter[0] + "'";
+                //mapView.when(() => {
+                //  this.mapLoaded.emit(true);
+                //});
+            }
+            catch (error) {
+                alert('We have an error: ' + error);
+            }
+        });
+    }
+    ngOnInit() {
+    }
+}
+SeedsCollectComponent.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: SeedsCollectComponent, deps: [], target: i0.ɵɵFactoryTarget.Component });
+SeedsCollectComponent.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "12.0.0", version: "12.2.3", type: SeedsCollectComponent, selector: "lib-SeedsCollect", inputs: { zz: "zz", workUnits: "workUnits" }, outputs: { mapLoaded: "mapLoaded" }, viewQueries: [{ propertyName: "content", first: true, predicate: ["mapViewNode"], descendants: true, static: true }], ngImport: i0, template: `
+    <div #mapViewNode style="width:100%;height: 100%;background-color:green"></div>
+  `, isInline: true });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: SeedsCollectComponent, decorators: [{
+            type: Component,
+            args: [{
+                    selector: 'lib-SeedsCollect',
+                    template: `
+    <div #mapViewNode style="width:100%;height: 100%;background-color:green"></div>
+  `,
+                    styles: []
+                }]
+        }], ctorParameters: function () { return []; }, propDecorators: { content: [{
+                type: ViewChild,
+                args: ['mapViewNode', { static: true }]
+            }], mapLoaded: [{
+                type: Output
+            }], zz: [{
+                type: Input
+            }], workUnits: [{
+                type: Input
+            }] } });
+
+class SeedsCollectModule {
+}
+SeedsCollectModule.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: SeedsCollectModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
+SeedsCollectModule.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: SeedsCollectModule, declarations: [SeedsCollectComponent], exports: [SeedsCollectComponent] });
+SeedsCollectModule.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: SeedsCollectModule, imports: [[]] });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "12.2.3", ngImport: i0, type: SeedsCollectModule, decorators: [{
+            type: NgModule,
+            args: [{
+                    declarations: [
+                        SeedsCollectComponent
+                    ],
+                    imports: [],
+                    exports: [
+                        SeedsCollectComponent
+                    ]
+                }]
+        }] });
+
+/*
+ * Public API Surface of work-unit
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { SeedsCollectComponent, SeedsCollectModule, SeedsCollectService, WorkUnitComponent, WorkUnitModule, WorkUnitService };
+//# sourceMappingURL=yaaranut-gis.js.map
