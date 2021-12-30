c7f686215e4931f0461f720649e3894041c73636
uriokkl <88084814+uriokkl@users.noreply.github.com>
2021-12-30 11:06:33 +0200

    Add files via upload

--- bundles/yaaranut-gis.umd.js
+++ bundles/yaaranut-gis.umd.js
@@ -862,7 +862,7 @@
                     try {
                         //esriConfig.apiKey = "AAPK9a3f55c380f94d1bb10a7566c7b32f941X_pcZKXmWY7Grjs6oA9AqufsDHrvRDYaOlUG8gvyD5fhZv-OGYyIgXEO-ihuO4T";
                         this.featerLayer = new FeatureLayer__default['default']({
-                            url: this.ys.apiUrl + "/ArcGIS/rest/services/ForestryTenders/FeatureServer/SubTenders/"
+                            url: this.ys.apiUrl + "/ArcGIS/rest/services/ForestryTenders/FeatureServer/1/"
                         });
                         this.featerLayer.opacity = 0.5;
                         this.featerLayer.definitionExpression = "1=2";
